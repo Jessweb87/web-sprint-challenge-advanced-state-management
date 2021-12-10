3650c88a652310c7e40da594089c35fa5eda015e
Jessweb87 <jprosper87@gmail.com>
2021-12-10 12:57:41 -0500

    completed MVP

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { addSmurfs, errorMessage } from './../actions';
+import { connect } from 'react-redux';
 
 const AddForm = (props) => {
     const [state, setState] = useState({
@@ -9,9 +11,8 @@ const AddForm = (props) => {
     });
 
     //remove when error state is added
-    const errorMessage = "";
 
-    const handleChange = e => {
+    const handleChange = (e) => {
         setState({
             ...state,
             [e.target.name]:e.target.value
@@ -22,10 +23,13 @@ const AddForm = (props) => {
         e.preventDefault();
         if (state.name === "" || state.position === "" || state.nickname === "") {
             //dispatch a custom error action
+            props.errorMessage("Name, position and nickname fields are required.");
         } else {
             //dispatch an addSmurf action
+            props.addSmurfs(state)
         }
     }
+
 
     return(<section>
         <h2>Add Smurf</h2>
@@ -47,14 +51,19 @@ const AddForm = (props) => {
                 <textarea onChange={handleChange} value={state.description} name="description" id="description" />
             </div>
             {
-                errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {errorMessage}</div>
+                props.errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {props.errorMessage}</div>
             }
-            <button>Submit Smurf</button>
+            <button onClick={handleSubmit}>Submit Smurf</button>
         </form>
     </section>);
 }
 
-export default AddForm;
+const mapStateToProps = (state) => {
+    return {
+        errorMessage: state.errorMessage
+    };
+};
+export default connect(mapStateToProps, {addSmurfs, errorMessage})(AddForm);
 
 //Task List:
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
